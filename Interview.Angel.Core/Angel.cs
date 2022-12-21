c06e08d8e18d2cdbd0686c0ad66b532eee2a783e
Anton <55918247+anonymousStyleKiller@users.noreply.github.com>
2022-12-21 20:12:53 +0200

    feature(core): add logic for
    Add twitch bot
    
    Closes (#2323)

---
+++ Interview.Angel.Core/Angel.cs
@@ -0,0 +1,32 @@
+﻿using Telegram.Bot;
+using TwitchLib.Api;
+using TwitchLib.Api.Core;
+using TwitchLib.Api.Services;
+using TwitchLib.Api.Services.Events.LiveStreamMonitor;
+
+namespace Interview.Angel.Core;
+
+public class Angel
+{
+    private readonly ITelegramBotClient _telegramBotClient;
+    private readonly LiveStreamMonitorService _liveStreamMonitorService;
+
+    public Angel(string telegramBotToken, string twitchApiToken, string twitchApiClientId)
+    {
+        _telegramBotClient = new TelegramBotClient(telegramBotToken);
+        _liveStreamMonitorService = new LiveStreamMonitorService(new TwitchAPI(settings: new ApiSettings
+        {
+            AccessToken = twitchApiToken,
+            ClientId = twitchApiClientId
+        }));
+
+        _liveStreamMonitorService.OnStreamOnline += OnStreamOnline;
+        _liveStreamMonitorService.Start();
+    }
+
+    private void OnStreamOnline(object? sender, OnStreamOnlineArgs onStreamOnlineArgs)
+    {
+        var game = onStreamOnlineArgs.Stream.GameName;
+        _telegramBotClient.SendTextMessageAsync(123123, $"Stream about {game}");
+    }
+}
---
+++ Interview.Angel.Core/TelegramBotMessagesHandler.cs
@@ -0,0 +1,18 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace Interview.Angel.Core;
+
+public class TelegramBotMessagesHandler : IUpdateHandler
+{
+    public Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
